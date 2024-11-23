41db8e87f98f7a0c5ca1ad07801bc3c6759c3012
Dinis Cruz <dinis.cruz@owasp.org>
2024-11-23 00:50:04 +0000

    refactored imports

--- osbot_markdown/markdown/Markdown_Parser.py
+++ osbot_markdown/markdown/Markdown_Parser.py
@@ -1,31 +1,29 @@
-from osbot_markdown.markdown.md__extensions.Extension__Chat_Bot         import Extension__Chat_Bot
-from osbot_markdown.markdown.md__extensions.Extension__IFrame           import Extension__IFrame
-from osbot_markdown.markdown.md__extensions.Extension__Mermaid          import Extension__Mermaid
-from osbot_markdown.markdown.md__extensions.Extension__QUnit            import Extension__QUnit
-#from osbot_markdown.markdown.md__extensions.Extension__Render_Template  import Extension__Render_Template
-from osbot_markdown.markdown.md__extensions.Extension__Video            import Extension__Video
-
-from markdown                                           import Markdown
 from osbot_utils.base_classes.Type_Safe                 import Type_Safe
 from osbot_utils.decorators.methods.cache_on_self       import cache_on_self
 
 
 class Markdown_Parser(Type_Safe):
 
-
     def markdown(self):
+        from markdown import Markdown
+
         return Markdown(extensions=self.extensions())
 
     @cache_on_self
     def extensions(self):
-
-        from markdown.extensions.attr_list   import AttrListExtension
-        from markdown.extensions.def_list    import DefListExtension
-        from markdown.extensions.footnotes   import FootnoteExtension
-        from markdown.extensions.md_in_html  import MarkdownInHtmlExtension
-        from markdown.extensions.meta        import MetaExtension
-        from markdown.extensions.tables      import TableExtension
-        from markdown.extensions.fenced_code import FencedCodeExtension
+        from osbot_markdown.markdown.md__extensions.Extension__Chat_Bot         import Extension__Chat_Bot
+        from osbot_markdown.markdown.md__extensions.Extension__IFrame           import Extension__IFrame
+        from osbot_markdown.markdown.md__extensions.Extension__Mermaid          import Extension__Mermaid
+        from osbot_markdown.markdown.md__extensions.Extension__QUnit            import Extension__QUnit
+        #from osbot_markdown.markdown.md__extensions.Extension__Render_Template  import Extension__Render_Template
+        from osbot_markdown.markdown.md__extensions.Extension__Video            import Extension__Video
+        from markdown.extensions.attr_list                                      import AttrListExtension
+        from markdown.extensions.def_list                                       import DefListExtension
+        from markdown.extensions.footnotes                                      import FootnoteExtension
+        from markdown.extensions.md_in_html                                     import MarkdownInHtmlExtension
+        from markdown.extensions.meta                                           import MetaExtension
+        from markdown.extensions.tables                                         import TableExtension
+        from markdown.extensions.fenced_code                                    import FencedCodeExtension
 
         markdown_default_extensions = [ AttrListExtension           (),
                                         DefListExtension            (),
@@ -59,6 +57,7 @@ class Markdown_Parser(Type_Safe):
     def parse(self, markdown_text):
         if markdown_text:
             markdown = self.markdown()
+
             html     = markdown.convert(markdown_text)
             meta     = self.extract_metadata_from_markdown(markdown)
         else:
@@ -71,5 +70,6 @@ class Markdown_Parser(Type_Safe):
         for key,value in markdown.Meta.items():  # this is from the MetaExtension
             metadata[key] = ' '.join(value)      # change
         return metadata
+
 
 markdown_parser = Markdown_Parser()
